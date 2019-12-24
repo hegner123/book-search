44ad5928d020af08cfa10af25af83905b5d2d43a
Michael Hegner <hegner123@gmail.com>
2019-12-24 14:50:16 -0500

    added api routes to saved page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Container";
 import Jumbotron from "../components/Jumbotron";
 import SavedBooks from '../components/SavedBooks';
+import API from "../utils/API";
 
 
 
@@ -21,29 +22,29 @@ class Search extends Component {
     this.loadBooks();
   }
 
-  // // Loads all books  and sets them to this.state.books
-  // loadBooks = () => {
-  //   API.getBooks()
-  //     .then(res =>
-  //       this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-  //     )
-  //     .catch(err => console.log(err));
-  // };
+  // Loads all books  and sets them to this.state.books
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
 
-  // // Deletes a book from the database with a given id, then reloads books from the db
-  // deleteBook = id => {
-  //   API.deleteBook(id)
-  //     .then(res => this.loadBooks())
-  //     .catch(err => console.log(err));
-  // };
+  // Deletes a book from the database with a given id, then reloads books from the db
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
 
-  // // Handles updating component state when the user types into the input field
-  // handleInputChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+  // Handles updating component state when the user types into the input field
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
   // // When the form is submitted, use the API.saveBook method to save the book data
   // // Then reload books from the database
